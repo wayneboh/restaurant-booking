3593671e4ee6702d6e1a72ce92c8531db2af3255
bigpotatsmallpotat <2000441b@student.tp.edu.sg>
2022-01-21 13:31:42 +0800

    Initial Commit

---
+++ maven.restaurantBooking/src/main/java/com/sddevops/maven/restaurantBooking/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.maven.restaurantBooking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
